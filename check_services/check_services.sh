c337574bee039cccbce2a5d7cfbf2035228d081f
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-04-02 19:41:58 +0200

    fix: minor fix on check-services

--- check_services/check_services.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if docker compose ps --services --filter "status=running" | grep -q -v homepage; then
-  echo "Non tutti i servizi sono in esecuzione."
-  exit 1
-fi
-curl -X POST services-discovery:5001/api/v1/homepage/refresh
